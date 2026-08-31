c993114a595d48df724020b6e3f187c555290ee2
AVULA VENKATA NAVEEN <naveenavula11@gmail.com>
2026-08-31 14:29:46 +0530

    ✅ Convert Sorted Array to Binary Search Tree | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 23 MB | 2026-08-31

---
+++ leetcode/easy/0108-convert-sorted-array-to-binary-search-tree/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        if (nums.empty()) return nullptr;
+        return createBST(nums, 0, static_cast<int>(nums.size()) - 1);
+    }
+
+    TreeNode* createBST(vector<int>& nums, int low, int high) {
+        if (low > high) return nullptr;
+
+        int mid = (low + high) / 2;
+        TreeNode* root = new TreeNode(nums[mid]);
+
+        root->left = createBST(nums, low, mid - 1);
+        root->right = createBST(nums, mid + 1, high);
+
+        return root;
+    }
+};

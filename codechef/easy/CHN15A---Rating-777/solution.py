f76973d1d4e7d7753361f6dd1af25e4cfb620cc6
AVULA VENKATA NAVEEN <naveenavula11@gmail.com>
2026-09-04 17:40:23 +0530

    ✅ CHN15A - Rating 777 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-04

--- codechef/easy/CHN15A---Rating-777/solution.py
+++ codechef/easy/CHN15A---Rating-777/solution.py
@@ -1,10 +1,10 @@
 # cook your dish here
 t=int(input())
 for _ in range(t):
-    a,b=map(int,input().split())
-    if a>b:
-        print(">")
-    elif a==b:
-        print("=")
-    else:
-        print("<")
+    n,k=map(int,input().split())
+    values=list(map(int,input().split()))
+    c=0
+    for x in values:
+        if (x+k)%7==0:
+            c+=1
+    print(c)

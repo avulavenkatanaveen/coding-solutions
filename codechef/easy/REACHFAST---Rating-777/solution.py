b37e2116ffb4cfe23023aa38ac799a58ee0fd14b
AVULA VENKATA NAVEEN <naveenavula11@gmail.com>
2026-09-05 19:58:59 +0530

    ✅ REACHFAST - Rating 777 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-05

--- codechef/easy/REACHFAST---Rating-777/solution.py
+++ codechef/easy/REACHFAST---Rating-777/solution.py
@@ -1,10 +1,7 @@
 # cook your dish here
 t=int(input())
 for _ in range(t):
-    n,k=map(int,input().split())
-    values=list(map(int,input().split()))
-    c=0
-    for x in values:
-        if (x+k)%7==0:
-            c+=1
-    print(c)
+    a,b,k=map(int,input().split())
+    d=abs(a-b)
+    s=(d+k-1)//k
+    print(s)

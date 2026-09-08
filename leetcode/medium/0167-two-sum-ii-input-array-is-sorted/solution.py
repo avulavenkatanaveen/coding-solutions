b40dd7b13dd4f5d12d3b6ceb4ed767e9aebf18e5
AVULA VENKATA NAVEEN <naveenavula11@gmail.com>
2026-09-08 10:48:42 +0530

    ✅ Two Sum II - Input Array Is Sorted | Medium | Python | Runtime: 0 ms (100.00%) | Memory: 20.4 MB | 2026-09-08

---
+++ leetcode/medium/0167-two-sum-ii-input-array-is-sorted/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left=0
+        right=len(numbers)-1
+        while left<right:
+            c_sum=numbers[left]+numbers[right]
+            if c_sum==target:
+                return [left+1,right+1]
+            elif c_sum<target:
+                left+=1
+            else:
+                right-=1
+

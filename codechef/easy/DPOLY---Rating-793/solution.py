4a045441728962345ef7da0a1c7d18b8905e039e
AVULA VENKATA NAVEEN <naveenavula11@gmail.com>
2026-09-11 19:49:18 +0530

    ✅ DPOLY - Rating 793 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-11

---
+++ codechef/easy/DPOLY---Rating-793/solution.py
@@ -0,0 +1,10 @@
+# cook your dish here
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    for i in range(n - 1, -1, -1):
+        if a[i] != 0:
+            print(i)
+            break
+    

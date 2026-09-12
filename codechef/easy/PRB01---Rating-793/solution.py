c301503ee4ecf627c545fc0fd82cb915137f6f26
AVULA VENKATA NAVEEN <naveenavula11@gmail.com>
2026-09-12 17:40:04 +0530

    ✅ PRB01 - Rating 793 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-12

---
+++ codechef/easy/PRB01---Rating-793/solution.py
@@ -0,0 +1,8 @@
+# cook your dish here
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    s=input().split()
+    start38_count=s.count("START38")
+    ltime_count=s.count("LTIME108")
+    print(start38_count,ltime_count)

9a587b00dfb0e091ab73881cfd672ee1bc152c9e
AVULA VENKATA NAVEEN <naveenavula11@gmail.com>
2026-09-15 21:28:35 +0530

    ✅ Rotate Image | Medium | Python | Runtime: 0 ms (100.00%) | Memory: 19.3 MB | 2026-09-15

---
+++ leetcode/medium/0048-rotate-image/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rotate(self, mat: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+
+        """
+        n=len(mat)
+        for i in range(n):
+            for j in range(i+1,n):
+                mat[i][j],mat[j][i]=mat[j][i],mat[i][j]
+        for row in mat:
+            row.reverse()

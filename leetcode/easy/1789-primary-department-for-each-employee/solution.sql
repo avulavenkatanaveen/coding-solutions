cb878202a96a623b1799df8fd704082484bc16a3
AVULA VENKATA NAVEEN <naveenavula11@gmail.com>
2026-09-21 11:07:56 +0530

    ✅ Primary Department for Each Employee | Easy | SQL | Runtime: 3040 ms (5.02%) | Memory: 0B | 2026-09-21

---
+++ leetcode/easy/1789-primary-department-for-each-employee/solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT employee_id,department_id from Employee
+WHERE primary_flag='Y' or
+employee_id in (
+    select employee_id from Employee group by employee_id having count(department_id)=1
+);
+
+
